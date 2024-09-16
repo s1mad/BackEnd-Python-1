9180ab208bbebf549ea2451772eed7672e38992d
simad <simanovich.artyom@gmail.com>
2024-09-16 20:01:13 +0300

    Pylint error correction

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+"""
+Cрипт, который выводит сообщение "Hello World!!!".
+"""
+
 def hello_world():
+    """Функция для вывода сообщения 'Hello World!!!'."""
     print("Hello World!!!")
 
 hello_world()
